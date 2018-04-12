4b90c53f08d2269cf4f4567e8108a831b0fb0c6c
qizl <q@qizl.cn>
2018-04-12 20:36:23 +0800

    -

--- SimpleTaskSystem/SimpleTaskSystem0726.Core/Tasks/Task.cs
+++ SimpleTaskSystem/SimpleTaskSystem0726.Core/Tasks/Task.cs
@@ -9,20 +9,25 @@ namespace SimpleTaskSystem.Tasks
     {
         public Task()
         {
-            CreationTime = DateTime.Now;
             State = TaskState.Active;
+            CreationTime = DateTime.Now;
         }
 
         public virtual int? AssignedPersonId { get; set; }
-        
         public virtual string Description { get; set; }
-
-        public virtual DateTime CreationTime { get; set; }
-
         public virtual TaskState State { get; set; }
+        public virtual DateTime CreationTime { get; set; }
 
         [ForeignKey("AssignedPersonId")]
         public virtual Person AssignedPerson { get; set; }
     }
-    
+
+    /// <summary>
+    /// Possible state of a <see cref="Task"/>
+    /// </summary>
+    public enum TaskState : byte
+    {
+        Active = 1,
+        Completed = 2
+    }
 }
--- SimpleTaskSystem/SimpleTaskSystem0726.EntityFramework/Migrations/201707261521459_InitialCreate.cs
+++ SimpleTaskSystem/SimpleTaskSystem0726.EntityFramework/Migrations/201707261521459_InitialCreate.cs
@@ -1,8 +1,8 @@
+using System.Data.Entity.Migrations;
+
 namespace SimpleTaskSystem0726.Migrations
 {
-    using System;
-    using System.Data.Entity.Migrations;
-    
+
     public partial class InitialCreate : DbMigration
     {
         public override void Up()
@@ -10,28 +10,28 @@ namespace SimpleTaskSystem0726.Migrations
             CreateTable(
                 "dbo.People",
                 c => new
-                    {
-                        Id = c.Int(nullable: false, identity: true),
-                        Name = c.String(),
-                    })
+                {
+                    Id = c.Int(nullable: false, identity: true),
+                    Name = c.String(),
+                })
                 .PrimaryKey(t => t.Id);
-            
+
             CreateTable(
                 "dbo.Tasks",
                 c => new
-                    {
-                        Id = c.Long(nullable: false, identity: true),
-                        AssignedPersonId = c.Int(),
-                        Description = c.String(),
-                        CreationTime = c.DateTime(nullable: false),
-                        State = c.Byte(nullable: false),
-                    })
+                {
+                    Id = c.Long(nullable: false, identity: true),
+                    AssignedPersonId = c.Int(),
+                    Description = c.String(),
+                    CreationTime = c.DateTime(nullable: false),
+                    State = c.Byte(nullable: false),
+                })
                 .PrimaryKey(t => t.Id)
                 .ForeignKey("dbo.People", t => t.AssignedPersonId)
                 .Index(t => t.AssignedPersonId);
-            
+
         }
-        
+
         public override void Down()
         {
             DropForeignKey("dbo.Tasks", "AssignedPersonId", "dbo.People");
