1b085326b6e771ecd9c1a7137cd77e0bb19022ed
qizl <q@qizl.cn>
2018-04-13 16:58:19 +0800

    -

---
+++ SimpleTaskSystemCore/test/SimpleTaskSystem.Tests/TestDatas/ProjectAppService_Tests.cs
@@ -0,0 +1,24 @@
+﻿using Shouldly;
+using SimpleTaskSystem.Tasks;
+using Xunit;
+
+namespace SimpleTaskSystem.Tests.TestDatas
+{
+    public class ProjectAppService_Tests : SimpleTaskSystemTestBase
+    {
+        private readonly IProjectAppService _projectAppService;
+
+        public ProjectAppService_Tests()
+        {
+            this._projectAppService = Resolve<IProjectAppService>();
+        }
+
+        [Fact]
+        public async System.Threading.Tasks.Task GetAllProjects()
+        {
+            var output = await this._projectAppService.GetAll(new Tasks.Dtos.GetAllProjectsInput());
+            
+            output.Items.Count.ShouldBe(0);
+        }
+    }
+}
